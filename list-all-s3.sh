10e14dbdbb8744300cb05b644c6d72a5d9981048
bhadra <bhadra@example.com>
2021-01-06 05:18:37 +0000

    updated script to show all bickets first and then to recursively print each bucket content separately

--- list-all-s3.sh
+++ list-all-s3.sh
@@ -2,13 +2,16 @@
 #Assuming AWS CLI is already installed, if not please uncomment below line 3 to install aws-cli on Ubuntu machine
 #sudo snap install aws-cli --classic
 #Exporting preconfigured AWS IAM account profile.
-#export AWS_PROFILE=[profile name, which you configured erlier)
+#export AWS_PROFILE=[profile name, which you configured earlier)
+printf "Listing ALL Buckets\n"
+aws s3 ls | awk '{if($NR >0) print $3}'
+echo "=========================================="
 my_array=($(aws s3 ls  --recursive --human-readable --summarize | awk '{ if($3 >0) print $3}'))
 for s3 in "${my_array[@]}"
 do
    location=($(aws s3api get-bucket-location --bucket $s3 --output text))
    echo $s3 "bucket region is" $location
-   printf "Listing content of bucket %s\n" "$s3"        
+   printf "Listing content of bucket %s\n" "$s3"
    aws s3 ls  s3://$s3 --recursive --human-readable --summarize
    echo "=========================================="
 done
